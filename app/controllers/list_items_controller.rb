44cee9bbbfb695a79381097517c077f9347cb5c9
edrain <edrain@gmail.com>
2013-01-28 20:42:42 -0800

    Misc small changes
    Return after sign-in
    Fix "View List" on search
    Paginate "View List"

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -1,10 +1,41 @@
 class ListItemsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: [:create, :destroy]
 
   def show
-    #@list_items = ListItem.find(params[:user_id])
-  end
+    @list_owner = User.find(params[:id])
+    
+    @list_shareable = @list_owner.shareable
+    
+    @list_editable = false
 
+    if user_signed_in?
+      if @list_owner.id = current_user.id
+        @list_editable = true
+      else
+        @list_editable = false
+      end
+    else
+      @list_editable = false  
+    end
+    
+    if @list_shareable == true or @list_editable == true
+      @display_list = true
+      @list_items = ListItem.where(params[:user_id]).paginate(:page => params[:page], :per_page => 10)
+      
+      @list_items.each do |artist|
+        @image = ITunesSearch.lookup_artist_image(:id => artist["artist_id"], :entity => "album")
+        @artist_info = ITunesSearch.lookup(:id => artist["artist_id"], :entity => "album")
+        artist.add_artist_image(@image)
+        artist.add_artist_name(@artist_info["artistName"]) 
+        artist.add_artist_url(@artist_info["artistLinkUrl"])                
+      end  
+    else
+      @display_list = false
+    end
+
+    session["user_return_to"] = request.fullpath          
+
+  end
 
   def create
     @list_item = ListItem.new( :user_id => current_user.id, :artist_id => params[:artist_id] )
@@ -17,6 +48,8 @@ class ListItemsController < ApplicationController
   end
   
   def destroy
+    ListItem.find( params[:id] ).destroy
+    redirect_to request.referer
   end
 
 end
